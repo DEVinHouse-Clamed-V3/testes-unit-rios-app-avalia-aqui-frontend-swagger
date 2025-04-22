4cb6de6d9c0202681da909789359867d98edd4a4
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2025-04-22 07:15:24 -0300

    fix: update test description for App rendering

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react-native";
 import App from "../App"; // ajuste o caminho conforme necessário
 
 describe("App", () => {
-    it("renders correctly", () => {
+    it("Renderização de App ate Presentation", () => {
         const { getByText } = render(<App />);
         // ajuste o texto conforme o conteúdo real do seu App
         expect(getByText("Avalia Aqui")).toBeTruthy();
